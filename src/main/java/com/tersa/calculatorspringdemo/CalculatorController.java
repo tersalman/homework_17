c25ecdd18f212f7f6ce2406457882d04fa5935c0
tersalman <mansalmanter@gmail.com>
2023-10-21 13:49:58 +0300

    homework was done :)

--- src/main/java/com/tersa/calculatorspringdemo/CalculatorController.java
+++ src/main/java/com/tersa/calculatorspringdemo/CalculatorController.java
@@ -20,26 +20,42 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "/plus")
-    public String plusNum(@RequestParam("Num1") Integer number1, @RequestParam("Num2") Integer number2) {
-    String a = Integer.toString(calculatorService.plusNum(number1, number2));
-    return "Вывод:"+ number1+ "+"+number2+ "="+ a;
+    public String plusNum(@RequestParam(name = "Num1",required = false) Integer number1,
+                          @RequestParam(name = "Num2",required = false) Integer number2) {
+        if (number1 == null || number2 == null) {
+            return "fill request in right form";
+        }
+        return calculatorService.plusNum(number1, number2);
     }
 
     @GetMapping(path = "/minus")
-    public String minusNum(@RequestParam("Num1") Integer number1, @RequestParam("Num2") Integer number2) {
-         String a = Integer.toString(calculatorService.minusNum(number1, number2));
-         return a;
+    public String minusNum(@RequestParam(name = "Num1",required = false) Integer number1,
+                           @RequestParam(name = "Num2",required = false) Integer number2) {
+        if (number1 == null || number2 == null) {
+            return "fill request in right form";
+        }
+        return calculatorService.minusNum(number1, number2);
     }
+
     @GetMapping(path = "/multiply")
-    public String multiplyNum(@RequestParam("Num1") Integer number1, @RequestParam("Num2") Integer number2) {
-        String a = Integer.toString(calculatorService.multiplyNum(number1, number2));
-        return a;
+    public String multiplyNum(@RequestParam(name = "Num1",required = false) Integer number1,
+                              @RequestParam(name = "Num2",required = false) Integer number2) {
+        if (number1 == null || number2 == null) {
+            return "fill request in right form";
+        }
+        return calculatorService.multiplyNum(number1, number2);
     }
+
     @GetMapping(path = "/devide")
-    public String devideNum(@RequestParam("Num1") Integer number1, @RequestParam("Num2") Integer number2) {
-        String b =calculatorService.check(number1,number2);
-         String a = Integer.toString(calculatorService.devideNum(number1, number2));
-         return b+a;
+    public String devideNum(@RequestParam(name = "Num1",required = false) Integer number1,
+                            @RequestParam(name = "Num2",required = false) Integer number2) {
+        if (number1 == null || number2 == null) {
+            return "fill request in right form";
+        }
+        if (number2.equals(0)) {
+            return "you cannot devide on a zero";
+        }
+        return calculatorService.devideNum(number1, number2);
 
     }
 
